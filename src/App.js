12fec972e4d1dca10500f7ad549948ff3765a263
Joao <joaopedro767767@gmail.com>
2022-07-14 12:51:53 -0300

    Ajuste

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './componente/Layout'
+import Sidebar from './componente/Sidebar'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Sidebar />} />
       </Routes>
     </>
   )
--- src/componente/Sidebar/index.js
+++ src/componente/Sidebar/index.js
@@ -4,8 +4,8 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
-  <div className="nac-bar">
-    <Link classname="logo" to="/">
+  <div className="nav-bar">
+    <Link className="logo" to="/">
       <img src={LogoS} alt="logo" />
       <img src={LogoSubtitle} alt="slobodan" />
     </Link>
